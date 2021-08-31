b5a0a7c6ea10474df6ae7c4b9c1bb40a12619db0
KaisKermani <kaiskermani@gmail.com>
2021-08-31 19:49:31 +0200

    create dev branch add template and bootstrap

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return {'msg': 'hello world'}
 
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'GET':
+        return render_template('file_uploader.html')
+    if request.method == 'POST':
+        return request.args
+        
 
 if __name__ == '__main__':
     app.run()
