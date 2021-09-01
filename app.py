0cf29cbc722c5969f9c435d951a27939b1ea7c0e
KaisKermani <kaiskermani@gmail.com>
2021-09-01 19:44:23 +0200

    dev Frontend

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, Response
+import json
 
 app = Flask(__name__)
 
@@ -12,8 +13,12 @@ def index():
 def upload_file():
     filenames = [file.filename for file in request.files.getlist('images')]
     print(filenames)
-    return render_template('index.html', filenames=filenames)
-        
+    resp = request.form.get('response')
+    if resp == "HTML":
+        return render_template('index.html', filenames=filenames)
+    elif resp == "JSON":
+        return Response(json.dumps(filenames, indent=2),  mimetype='application/json')
+
 
 if __name__ == '__main__':
     app.run()
